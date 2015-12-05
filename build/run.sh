7371e215c2f7c57da47b2084800aed663baffb45
Kyle <divideandconquer@users.noreply.github.com>
2015-12-05 11:08:50 -0500

    adding check for failed build

--- build/run.sh
+++ build/run.sh
@@ -12,7 +12,7 @@ CurrentDir=`pwd`
 ServicePath="${CurrentDir/$BasePath/}"
 
 # run the build
-docker run -it -v `pwd`:"/go/src/$ServicePath" divideandconquer/godep:1.5.1 /bin/bash -c "cd /go/src/$ServicePath; ./build/build.sh"
+docker run -it -v `pwd`:"/go/src/$ServicePath" divideandconquer/godep:1.5.1 /bin/bash -c "cd /go/src/$ServicePath; ./build/build.sh" || { echo 'build failed' ; exit 1; }
 
 # build the docker container with the new binary
 docker build -t $ServiceName .
