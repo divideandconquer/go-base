3bdff66c6318bdc6173680e72c9efda1b35f317d
Kyle Boorky <kyle.boorky@gmail.com>
2015-12-05 12:19:31 -0500

    adding config import on run

--- build/run.sh
+++ build/run.sh
@@ -14,6 +14,13 @@ ServicePath="${CurrentDir/$BasePath/}"
 # run the build
 docker run -it -v `pwd`:"/go/src/$ServicePath" divideandconquer/godep:1.5.1 /bin/bash -c "cd /go/src/$ServicePath; ./build/build.sh" || { echo 'build failed' ; exit 1; }
 
+# update the config 
+ConsulAddr=`ifconfig eth1 | grep "inet " | sed -e 's/^[[:space:]]*//' | cut -d" " -f2`
+ConfigPath="`pwd`/config/dev.json"
+docker run -v $ConfigPath:/config.json divideandconquer/go-consul-client  -file /config.json -namespace dev/$ServiceName -consul $ConsulAddr:8500
+
+
+
 # build the docker container with the new binary
 docker build -t $ServiceName .
 
